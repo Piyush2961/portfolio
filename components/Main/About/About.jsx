5074afedc5cebdff088265f07e3c2320e1917c36
Piyush Kumar <pikumar@mafcarrefour.com>
2022-06-18 02:37:52 +0530

    Added About Me Section

---
+++ components/Main/About/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./About.module.css";
+
+const About = () => {
+  return (
+    <div className={styles.main}>
+      <h1>About <span style={{ color: "red" }}>Me</span> </h1>
+      <p>
+        I'm a FULL Stack (MERN) Web Developer. Currently I am working on Java
+        Springboot. I'm a Pre - Final Year Student pursuing B.E. (Bachelor of
+        Engineering).
+      </p>
+      <p>
+        Most recent Experience is that currently I'm working as an SDE intern at
+        MAF - Carrefour. Previously I have worked with StockPe and Admere
+        Selvyn.
+      </p>
+      <p>I am very enthusiastic and ready to explore and learn with the advancement of Technology.</p>
+    </div>
+  );
+};
+
+export default About;
--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import styles from './Main.module.css'
 import Heading from './Heading/Heading'
+import About from './About/About'
 
 const Main = () => {
   return (
     <div className={styles.main}>
       <Heading />
+      <About />
     </div>
   )
 }
