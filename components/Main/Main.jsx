f3a7d494369db77376c24b2f95a66763bb5e31a9
Piyush Kumar <pikumar@mafcarrefour.com>
2022-06-18 01:04:31 +0530

    fixed image main

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -5,42 +5,6 @@ const Main = () => {
   return (
     <div className={styles.main}>
       <Heading />
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
-      <h1>
-        This is the main page
-      </h1>
     </div>
   )
 }
